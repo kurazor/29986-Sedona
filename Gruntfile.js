e45a048fc739db443a8f7dcd317f28d8fcc9235a
kurazor <peluhiny@gmail.com>
2015-03-26 22:36:59 +0300

    добавлена поддержка минификации svg

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,38 @@
 module.exports = function(grunt) {
-
+  var getSvgFileNames = function() {
+    return [
+      'form_logo_triangle.svg',
+      'icon_calendar.svg',
+      'icon_cross.svg',
+      'icon_facebook.svg',
+      'icon_gift.svg',
+      'icon_house.svg',
+      'icon_menu.svg',
+      'icon_minus.svg',
+      'icon_plus.svg',
+      'icon_twitter.svg',
+      'icon_youtube.svg',
+      'logo_glukhanko.svg',
+      'logo_htmlacademy.svg',
+      'text_becausecanyon.svg',
+      'text_sedona.svg',
+      'text_tothegorgeous.svg',
+      'text_welcome.svg',
+    ];
+  };
+      
+  var getSvgMinFiles = function(sourceFolder, targetFolder) {
+    var fileNames = getSvgFileNames();
+    var result = {};
+    for (var index = 0; index < fileNames.length; index++) {
+      var fileName = fileNames[index];
+      var sourceFile = sourceFolder + "/" + fileName;
+      var targetFile = targetFolder + "/" + fileName;
+      result[targetFile] = sourceFile;
+    }
+    return result;
+  };
+      
   require('load-grunt-tasks')(grunt);
 
   grunt.initConfig({
@@ -94,9 +127,24 @@ module.exports = function(grunt) {
       all : {
         options: {},
         files: {
-           'js/svginjector.js': ['_sources/svg/*.svg'],
+           'js/svginjector.js': ['_sources/svg/min/*.svg'],
         }
       }
+    },
+    
+    svgmin: {
+        options: {
+            plugins: [
+                {
+                    removeViewBox: false
+                }, {
+                    removeUselessStrokeAndFill: false
+                }
+            ]
+        },
+        dist: {
+            files: getSvgMinFiles('_sources/svg', '_sources/svg/min')
+        }
     }
 
   });
@@ -120,6 +168,7 @@ module.exports = function(grunt) {
     
   grunt.registerTask('images', [
     /*'grunticon:images',*/
+    'svgmin',
     'svginject:all'
   ]);
 };
