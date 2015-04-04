1e5ffca881e64b076d0fa169b4279c3c720568b2
kurazor <peluhiny@gmail.com>
2015-04-04 11:30:23 +0300

    добавлена интерактивная карта

--- sources/js/script.js
+++ sources/js/script.js
@@ -1,6 +1,4 @@
 (function() {
-
-  /*grunticon(["css/sprites/icons-svg.css", "css/sprites/icons-png.css", "css/sprites/icons-fallback.css"]);*/
   
   (function() {
     var menu = document.querySelector('.page-menu');
@@ -18,6 +16,18 @@
       if (!menu.classList.contains('page-menu--collapsed'))
         menu.classList.add('page-menu--collapsed');
     });    
+  })();
+  
+  (function() {
+    var mapWrapper = document.querySelector('.page-footer__map');
+    var mapOptions = {
+      center: new google.maps.LatLng(34.5856169, -112.0083956),
+      zoom: 8,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    
+    var map = new google.maps.Map(mapWrapper, mapOptions);
+    mapWrapper.classList.add('page-footer__map--no-image');
   })();
   
 })();
